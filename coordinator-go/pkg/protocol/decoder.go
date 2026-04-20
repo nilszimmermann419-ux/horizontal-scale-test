93574066b9076a0412b593ceb7a1338eba807010
nilszimmermann419-ux <nilszimmermann419@gmail.com>
2026-04-20 21:40:26 +0200

    security: Add bounds checking to packet decoder
    - Add bounds checks before every read operation
    - Validate string/bytes lengths against remaining buffer
    - Return errors instead of panicking on malformed packets
    - Add maximum packet size limit (16MB)
    - Prevent server crashes from malicious packets

--- coordinator-go/pkg/protocol/decoder.go
+++ coordinator-go/pkg/protocol/decoder.go
@@ -2,7 +2,16 @@ package protocol
 
 import (
 	"encoding/binary"
+	"errors"
 	"math"
+)
+
+const MaxPacketSize = 16 * 1024 * 1024 // 16MB max packet size
+
+var (
+	ErrBufferUnderflow = errors.New("buffer underflow")
+	ErrPacketTooLarge  = errors.New("packet too large")
+	ErrInvalidLength   = errors.New("invalid string/bytes length")
 )
 
 type PacketDecoder struct {
@@ -10,68 +19,109 @@ type PacketDecoder struct {
 	pos int
 }
 
-func NewDecoder(buf []byte) *PacketDecoder {
+func NewDecoder(buf []byte) (*PacketDecoder, error) {
+	if len(buf) > MaxPacketSize {
+		return nil, ErrPacketTooLarge
+	}
 	return &PacketDecoder{
 		buf: buf,
 		pos: 0,
-	}
+	}, nil
 }
 
-func (d *PacketDecoder) ReadUint8() uint8 {
+func (d *PacketDecoder) ReadUint8() (uint8, error) {
+	if d.pos+1 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := d.buf[d.pos]
 	d.pos++
-	return v
+	return v, nil
 }
 
-func (d *PacketDecoder) ReadUint16() uint16 {
+func (d *PacketDecoder) ReadUint16() (uint16, error) {
+	if d.pos+2 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := binary.LittleEndian.Uint16(d.buf[d.pos:])
 	d.pos += 2
-	return v
+	return v, nil
 }
 
-func (d *PacketDecoder) ReadUint32() uint32 {
+func (d *PacketDecoder) ReadUint32() (uint32, error) {
+	if d.pos+4 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := binary.LittleEndian.Uint32(d.buf[d.pos:])
 	d.pos += 4
-	return v
+	return v, nil
 }
 
-func (d *PacketDecoder) ReadUint64() uint64 {
+func (d *PacketDecoder) ReadUint64() (uint64, error) {
+	if d.pos+8 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := binary.LittleEndian.Uint64(d.buf[d.pos:])
 	d.pos += 8
-	return v
+	return v, nil
 }
 
-func (d *PacketDecoder) ReadFloat32() float32 {
+func (d *PacketDecoder) ReadFloat32() (float32, error) {
+	if d.pos+4 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := binary.LittleEndian.Uint32(d.buf[d.pos:])
 	d.pos += 4
-	return math.Float32frombits(v)
+	return math.Float32frombits(v), nil
 }
 
-func (d *PacketDecoder) ReadFloat64() float64 {
+func (d *PacketDecoder) ReadFloat64() (float64, error) {
+	if d.pos+8 > len(d.buf) {
+		return 0, ErrBufferUnderflow
+	}
 	v := binary.LittleEndian.Uint64(d.buf[d.pos:])
 	d.pos += 8
-	return math.Float64frombits(v)
+	return math.Float64frombits(v), nil
 }
 
-func (d *PacketDecoder) ReadString() string {
-	length := d.ReadUint16()
+func (d *PacketDecoder) ReadString() (string, error) {
+	length, err := d.ReadUint16()
+	if err != nil {
+		return "", err
+	}
+	if int(length) < 0 || d.pos+int(length) > len(d.buf) {
+		return "", ErrInvalidLength
+	}
 	v := string(d.buf[d.pos : d.pos+int(length)])
 	d.pos += int(length)
-	return v
+	return v, nil
 }
 
-func (d *PacketDecoder) ReadBytes() []byte {
-	length := d.ReadUint32()
-	v := d.buf[d.pos : d.pos+int(length)]
+func (d *PacketDecoder) ReadBytes() ([]byte, error) {
+	length, err := d.ReadUint32()
+	if err != nil {
+		return nil, err
+	}
+	if length > uint32(MaxPacketSize) {
+		return nil, ErrInvalidLength
+	}
+	if int(length) < 0 || d.pos+int(length) > len(d.buf) {
+		return nil, ErrInvalidLength
+	}
+	v := make([]byte, length)
+	copy(v, d.buf[d.pos:d.pos+int(length)])
 	d.pos += int(length)
-	return v
+	return v, nil
 }
 
 func (d *PacketDecoder) Remaining() int {
 	return len(d.buf) - d.pos
 }
 
-func (d *PacketDecoder) Reset(buf []byte) {
+func (d *PacketDecoder) Reset(buf []byte) error {
+	if len(buf) > MaxPacketSize {
+		return ErrPacketTooLarge
+	}
 	d.buf = buf
 	d.pos = 0
+	return nil
 }
